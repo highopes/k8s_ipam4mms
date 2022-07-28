ce6ed0917fe5f549284b21ec2f2784f45e644f82
highopes <weihanghank@gmail.com>
2022-07-28 10:59:58 +0800

    first commit

---
+++ assign_ippool.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+###################################################################################
+#                           Written by Wei, Hang                                  #
+#                          weihanghank@gmail.com                                  #
+###################################################################################
+"""
+Set all pods in a deployment to addresses in a specific IP pool
+"""
+
+from my_py.configbyssh import *
+import json
+
+HOST_INFO = {"host": "10.75.53.43", "port": "22", "user": "root", "pass": "cisco123"}
+
+NAMESPACE = "default"
+DEPLOYMENT = "myweb"
+IPPOOL = "ip250"
+CMD = '''
+kubectl patch deployment {} -n {} -p '{{"spec": {{"template":{{"metadata":{{"annotations":{{"cni.projectcalico.org/ipv4pools":"[\\"{}\\"]"}}}}}}}} }}'
+'''.format(DEPLOYMENT, NAMESPACE, IPPOOL)
+print(CMD)
+
+
+def main():
+    """
+    Push the commands and get the response
+    """
+    # result_dict = {}
+    # result_dict = json.loads(configbyssh(HOST_INFO, CMD))
+    print(bytes.decode(configbyssh(HOST_INFO, CMD)))
+
+
+if __name__ == '__main__':
+    main()
---
+++ delete_ippool.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+###################################################################################
+#                           Written by Wei, Hang                                  #
+#                          weihanghank@gmail.com                                  #
+###################################################################################
+"""
+Push any command through SSH, and get response
+"""
+
+from my_py.configbyssh import *
+import json
+
+HOST_INFO = {"host": "10.75.53.43", "port": "22", "user": "root", "pass": "cisco123"}
+SUBNET = "10.244.252.0/24"
+NAME = "ip252"
+CMD = '''
+cat <<EOF | calicoctl delete -f -
+apiVersion: projectcalico.org/v3
+kind: IPPool
+metadata:
+  name: {}
+spec:
+  cidr: {}
+  ipipMode: Never
+  natOutgoing: false
+  nodeSelector: all()
+  vxlanMode: Never
+  disabled: false
+EOF
+'''.format(NAME, SUBNET)
+print(CMD)
+
+
+def main():
+    """
+    Push the commands and get the response
+    """
+    # result_dict = {}
+    # result_dict = json.loads(configbyssh(HOST_INFO, CMD))
+    print(bytes.decode(configbyssh(HOST_INFO, CMD)))
+
+
+if __name__ == '__main__':
+    main()
