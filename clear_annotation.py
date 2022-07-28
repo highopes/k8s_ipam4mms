967a4360a09a6a97cd9f74198d284989314ba226
highopes <weihanghank@gmail.com>
2022-07-28 17:52:29 +0800

    bug fix

--- clear_annotation.py
+++ clear_annotation.py
@@ -13,12 +13,16 @@ import json
 # HOST_INFO import from configbyssh module
 
 NAMESPACE = "default"  # name of k8s namespace
-DEPLOYMENT = "myweb"   # name of k8s deployment
-IPPOOL = "ip250"       # name of calico ipam IP Pool
-CMD = '''
+DEPLOYMENT = "myweb"  # name of k8s deployment
+IPPOOL = "ip250"  # name of calico ipam IP Pool
+
+CMD1 = '''
+kubectl get deployment {} -n {} -o jsonpath={{.spec.template.metadata.annotations."cni\.projectcalico\.org/ipv4pools"}}
+'''.format(DEPLOYMENT, NAMESPACE)
+CMD2 = '''
 kubectl patch deployment {} -n {} --type=json -p='[{{"op":"remove","path":"/spec/template/metadata/annotations/cni.projectcalico.org~1ipv4pools"}}]'
 '''.format(DEPLOYMENT, NAMESPACE)
-print(CMD)
+print(CMD2)
 
 
 def main():
@@ -27,7 +31,10 @@ def main():
     """
     # result_dict = {}
     # result_dict = json.loads(configbyssh(HOST_INFO, CMD))
-    print(bytes.decode(configbyssh(HOST_INFO, CMD)))
+    if configbyssh(HOST_INFO, CMD1):
+        print(bytes.decode(configbyssh(HOST_INFO, CMD2)))
+    else:
+        print("No such annotation")
 
 
 if __name__ == '__main__':
